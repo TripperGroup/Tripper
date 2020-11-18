8fd6aeee5350a7e3c0ab720cfc3dcbca9fabbe14
mohammad maso <mohammadmasoudie@gmail.com>
2020-11-18 23:23:25 +0330

    feat: register models

--- trip/admin.py
+++ trip/admin.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from .models import Image, Trip, TripCategory, TripActivities, Review, TripPlaces, Companions, Place
 
 
-# # Register your models here.
-# class ImageAdmin(admin.ModelAdmin):
-#     readonly_fields = ('latitud','longitud')
-#     list_display = ('id','subject', 'image') # To show as readable list 
+# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ('latitud','longitud')
+    list_display = ('id','subject', 'image') # To show as readable list 
 
-# admin.site.register(Review)
+admin.site.register(Review)
 
-# admin.site.register(Trip)
-# admin.site.register(TripCategory)
-# admin.site.register(TripActivities)
-# admin.site.register(TripPlaces)
-# admin.site.register(Image,ImageAdmin)
-# admin.site.register(Companions)
-# admin.site.register(Place)
+admin.site.register(Trip)
+admin.site.register(TripCategory)
+admin.site.register(TripActivities)
+admin.site.register(TripPlaces)
+admin.site.register(Image,ImageAdmin)
+admin.site.register(Companions)
+admin.site.register(Place)
 
 
 
